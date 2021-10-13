b53f52fa10143fbe86e984dcfa1fb91ef6dbe244
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-13 11:18:36 -0400

    apping API to public files

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+const API = require("./api");
+
 init();
 
 async function init() {
@@ -6,8 +8,7 @@ async function init() {
     if (workout) {
       location.search = "?id=" + workout._id;
     } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
+      document.querySelector("#continue-btn").classList.add("d-none");
     }
   }
 }
-
