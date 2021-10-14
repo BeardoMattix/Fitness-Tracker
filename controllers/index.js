5bee08882744c5efabf76e9d526b8f7e79c091e1
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-14 10:52:43 -0400

    troubleshooting issue with homeroutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 // Importing the router.
-const apiRoutes = require("./api");
 const router = require("express").Router();
 
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes.js");
+
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
