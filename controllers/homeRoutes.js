5dddfc9ca6aa9ef5cf45b5370e237b87408d7ce9
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-14 10:44:03 -0400

    created first home route for the index html path

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,7 @@
 //TODO: Update this with the home routes
 const router = require("express").Router();
 const path = require("path");
+
+router.get("/", ({ res }) => {
+  res, sendFile(path.join(__dirname, "../public/index.html"));
+});
