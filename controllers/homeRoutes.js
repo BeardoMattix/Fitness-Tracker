bddc5e75b2550426e41bcef4fbee92e8e2979fd3
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-14 10:47:11 -0400

    updated stats route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,13 @@
 const router = require("express").Router();
 const path = require("path");
 
+// These routes allow the router to access the html pages for the root index, the exercise page, and the stats page.
 router.get("/", ({ res }) => {
   res, sendFile(path.join(__dirname, "../public/index.html"));
 });
 router.get("/exercise", ({ res }) => {
   res, sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+router.get("/stats", ({ res }) => {
+  res, sendFile(path.join(__dirname, "../public/stats.html"));
 });
